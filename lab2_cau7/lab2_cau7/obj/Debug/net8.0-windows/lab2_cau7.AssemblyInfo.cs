ed22cec43bf5ee9a0007c33b2d8c85857229ba05
lebuinguyenhai25 <lebuinguyenhai25@gmail.com>
2024-04-10 00:21:04 +0700

    lab2

--- lab2_cau7/lab2_cau7/obj/Debug/net8.0-windows/lab2_cau7.AssemblyInfo.cs
+++ lab2_cau7/lab2_cau7/obj/Debug/net8.0-windows/lab2_cau7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2_cau7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82b5d9f3842542f5532cf5d5635c929638718d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2_cau7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2_cau7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
